2d7219f5b7d45aa58ee9a20b251437852eb68d63
Peter Clark Mosbacher <peter.c.mosbacher@nasa.gov>
2013-07-18 07:07:03 -0500

    First DB-based View

--- PedalBus/Controllers/HomeController.cs
+++ PedalBus/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PedalBus.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace PedalBus.Controllers
 {
     public class HomeController : Controller
     {
+        PedalBusDb _db = new PedalBusDb();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,11 @@ namespace PedalBus.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "All your vital information.";
 
-            return View();
+            var model = _db.People.Where(a => a.DomainID == User.Identity.Name).SingleOrDefault();
+
+            return View(model);
         }
 
         public ActionResult Contact()
@@ -32,6 +37,15 @@ namespace PedalBus.Controllers
             ViewBag.Message = "I'm not sure there is any help for you.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
