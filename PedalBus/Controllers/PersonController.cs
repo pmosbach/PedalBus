10fc565e19f0aace4792976e2d674c8321fc1137
Peter Clark Mosbacher <peter.c.mosbacher@nasa.gov>
2013-07-20 17:00:04 -0500

    Beginning of Person Controller

---
+++ PedalBus/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using PedalBus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PedalBus.Controllers
+{
+    public class PersonController : Controller
+    {
+        PedalBusDb _db = new PedalBusDb();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
